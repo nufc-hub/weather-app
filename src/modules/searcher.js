20ad18a9d5463720f5ed20eef98b7f1d7fe68668
nufc-hub <kmchenouf@gmail.com>
2024-04-10 13:13:00 +0900

    Add onSearch functionality

--- src/modules/searcher.js
+++ src/modules/searcher.js
@@ -1,17 +1,32 @@
 class Searcher {
   constructor(searchElement) {
     this.searchElement = searchElement;
+
+    // Makes sure that the 'this' inside the onSearch function always refers to the instance of the Search class.
+    this.onSearch = this.onSearch.bind(this);
   }
 
   // Gets value of search bar input.
   getSearchValue() {
-    const searchValue = this.searchElement.value;
-    return searchValue;
+    return this.searchElement.value;
   }
 
   // Clears the search bar.
   clearSearchValue() {
     this.searchElement.value = '';
+  }
+
+  // Function to trigger search functionality.
+  onSearch(callback) {
+    this.searchElement.addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+        const searchValue = this.getSearchValue();
+        if (searchValue !== '') {
+          callback(searchValue);
+        }
+        this.clearSearchValue();
+      }
+    });
   }
 }
 
