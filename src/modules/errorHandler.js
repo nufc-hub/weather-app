fd5e72e3bcf91fe30e52462097857ea33f0c74be
nufc-hub <kmchenouf@gmail.com>
2024-04-18 22:16:29 +0900

    Add handleErrors function

--- src/modules/errorHandler.js
+++ src/modules/errorHandler.js
@@ -14,6 +14,40 @@ class ErrorHandler {
   clearError() {
     this.errorElement.textContent = '';
   }
+
+  // Function for handling the errors.
+  handleErrors(error) {
+    if (error.message === 'No data found for the provided query') {
+      // Error message user receives.
+      this.displayError(
+        `Sorry, we couldn't find any weather data for the entered city. Please check the spelling or try another city.`
+      );
+      // Console error message.
+      console.error(`TypeError occurred: ${error.message}`);
+    }
+    if (error instanceof TypeError) {
+      // Error message user receives.
+      this.displayError(
+        `There has been an error. Our team is currently working to fix the problem.`
+      );
+      // Console error message.
+      console.error(`TypeError occurred: ${error.message}`);
+    } else if (error instanceof SyntaxError) {
+      // Error message user receives.
+      this.displayError(
+        `There has been an error. Our team is currently working to fix the problem.`
+      );
+      // Console error message.
+      console.error(`SyntaxError occurred: ${error.message}`);
+    } else {
+      // Error message user receives.
+      this.displayError(
+        `Sorry, we couldn't find any weather data for the entered city. Please check the spelling or try another city.`
+      );
+      // Console error message.
+      console.error(`An error has occurred: ${error.message}`);
+    }
+  }
 }
 
 export default ErrorHandler;
