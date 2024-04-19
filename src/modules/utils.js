41a945940e0e174a7973ec0c2281b5c35b14d0ed
nufc-hub <kmchenouf@gmail.com>
2024-04-19 19:47:27 +0900

    Add getSelectors function

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,3 +1,46 @@
+function getSelectors() {
+  // Define selectors. These are the HTML elements.
+  const selectors = {
+    // Weather and weather details.
+    city: 'city-name',
+    country: 'country-name',
+    temperature: 'temperature-number',
+    weather: 'weather-type',
+    feelsLike: 'feels-like',
+    wind: 'wind',
+    barometer: 'barometer',
+    visibility: 'visibility',
+    humidity: 'humidity',
+    precipitation: 'precipitation',
+    firstForecastDay: 'first-forecast-day',
+    secondForecastDay: 'second-forecast-day',
+    thirdForecastDay: 'third-forecast-day',
+    firstForecastHigh: 'first-forecast-high',
+    secondForecastHigh: 'second-forecast-high',
+    thirdForecastHigh: 'third-forecast-high',
+    firstForecastLow: 'first-forecast-low',
+    secondForecastLow: 'second-forecast-low',
+    thirdForecastLow: 'third-forecast-low',
+    firstForecastIcon: 'first-forecast-icon',
+    secondForecastIcon: 'second-forecast-icon',
+    thirdForecastIcon: 'third-forecast-icon',
+
+    // Search element.
+    searchElement: 'search',
+
+    // Error Element.
+    errorElement: 'search-error-message',
+
+    // Celsius unit button.
+    celsiusButton: 'celsius-button',
+
+    // Fahrenheit unit button.
+    fahrenheitButton: 'fahrenheit-button',
+  };
+
+  return selectors;
+}
+
 // Returns named day of the week.
 function dayGetter(timestamp) {
   const daysOfWeek = [
@@ -15,4 +58,13 @@ function dayGetter(timestamp) {
   return daysOfWeek[dayOfWeekIndex];
 }
 
-export default dayGetter;
+// Function for building the weather URL.
+function buildWeatherURL(config, searchValue) {
+  const { weatherURL } = config;
+  //  Build the URL
+  const weatherDataURl = `${weatherURL.weatherForecastURL}?key=${weatherURL.apiKey}&q=${searchValue}&days=${weatherURL.days}`;
+  console.log(weatherDataURl);
+  return weatherDataURl;
+}
+
+export { getSelectors, dayGetter, buildWeatherURL };
