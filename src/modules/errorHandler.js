aa55edcb0c17ac7d2d6d9e9fddab116b8c448d45
nufc-hub <kmchenouf@gmail.com>
2024-04-24 22:22:34 +0900

    Modify error message text content

--- src/modules/errorHandler.js
+++ src/modules/errorHandler.js
@@ -20,29 +20,25 @@ class ErrorHandler {
     if (error.message === 'No data found for the provided query') {
       // Error message user receives.
       this.displayError(
-        `Sorry, we couldn't find any weather data for the entered city. Please check the spelling or try another city.`
+        `No weather data found for that city. Please try another.`
       );
       // Console error message.
       console.error(`TypeError occurred: ${error.message}`);
     }
     if (error instanceof TypeError) {
       // Error message user receives.
-      this.displayError(
-        `There has been an error. Our team is currently working to fix the problem.`
-      );
+      this.displayError(`Oops! Something went wrong. We're on it!`);
       // Console error message.
       console.error(`TypeError occurred: ${error.message}`);
     } else if (error instanceof SyntaxError) {
       // Error message user receives.
-      this.displayError(
-        `There has been an error. Our team is currently working to fix the problem.`
-      );
+      this.displayError(`Oops! Something went wrong. We're on it!`);
       // Console error message.
       console.error(`SyntaxError occurred: ${error.message}`);
     } else {
       // Error message user receives.
       this.displayError(
-        `Sorry, we couldn't find any weather data for the entered city. Please check the spelling or try another city.`
+        `No weather data found for that city. Please try another.`
       );
       // Console error message.
       console.error(`An error has occurred: ${error.message}`);
